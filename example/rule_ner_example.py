53f38b556510255214f1cafba3e2f517cf1a455c
MJ-Jang <xkxpa@korea.ac.kr>
2020-05-11 10:20:53 +0900

    update data

--- example/rule_ner_example.py
+++ example/rule_ner_example.py
@@ -2,8 +2,8 @@ from reree.entity_extractor import ReReeExtractor
 from reree.helper import get_regex_data, get_regex_combination
 
 
-reg_pattern = get_regex_data('reree/data/rule.md')
-comb_pattern = get_regex_combination('reree/data/combination.yml')
+reg_pattern = get_regex_data('data/rule.md')
+comb_pattern = get_regex_combination('data/combination.yml')
 
 
 extractor = ReReeExtractor(reg_pattern, comb_pattern)
