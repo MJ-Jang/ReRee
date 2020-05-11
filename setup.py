e1eaff832e7a7fae1a4824484ad570f674e542b8
MJ-Jang <xkxpa@korea.ac.kr>
2020-05-11 10:28:54 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
 setup(
     name='reree',
     version='0.1',
     description='REgex Rule Entity Extractor',
     author='MJ Jang',
     install_requires=[],
-    packages=find_packages(exclude=['docs', 'tests', 'tmp']),
+    packages=find_packages(exclude=['docs', 'tests', 'tmp', 'data']),
     python_requires='>=3',
     zip_safe=False,
     include_package_data=True
