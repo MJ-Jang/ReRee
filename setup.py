f4632d75f3af000fb022178da86d094f97d89edf
MJ-Jang <xkxpa@korea.ac.kr>
2020-05-20 14:32:21 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name='reree',
-    version='0.1.2',
+    version='0.1.3',
     description='REgex Rule Entity Extractor',
     author='MJ Jang',
     install_requires=[],
