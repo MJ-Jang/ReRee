5e6ba3c55e65734dc3d05063c356b8244de8e300
MJ-Jang <xkxpa@korea.ac.kr>
2020-06-22 15:37:34 +0900

    fix pattern

--- example/rule_ner_example.py
+++ example/rule_ner_example.py
@@ -2,11 +2,10 @@ from reree.entity_extractor import ReReeExtractor
 from reree.helper import get_regex_data, get_regex_combination
 
 
-reg_pattern = get_regex_data('data/regex.md')
-comb_pattern = get_regex_combination('data/regex_combination.yml')
+# reg_pattern = get_regex_data('data/regex.md')
+# comb_pattern = get_regex_combination('data/regex_combination.yml')
 
 
 extractor = ReReeExtractor()
-a = extractor.process('다음 달 1일을 시작일자로 해서 이주일 동안 로밍 요금제 쓸게요')
-
-
+a = extractor.process("일월 해 줘")
+a
